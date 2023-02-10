d300b3a8d08d9a429f8e1d8c8a337df7947e53b5
LuciaCembellin <124940104+LuciaCembellin5@users.noreply.github.com>
2023-02-10 11:31:36 +0100

    Update Ejemplo.java
    Modificación local de Ejemplo.java

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,5 +1,6 @@
 public class Ejemplo {
     public static void main(String[] args) {
-        System.out.println("Versión inicial");
+        System.out.println("Cambios locales " + 
+                            "que ignoran la edición remota");
     }
 }
